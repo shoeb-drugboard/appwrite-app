426cd16da54ff613a903c33f950b75d366241b39
shoeb-drugboard <frontend@drugboard.ai>
2025-02-27 02:03:30 +0530

    feat: add user display alongside product listings

--- src/components/ProductDisplay.tsx
+++ src/components/ProductDisplay.tsx
@@ -6,26 +6,51 @@ type Product = {
     price: number;
     image: string;
 }
+type User = {
+    name: string;
+    email: string;
+    LoggedIn: boolean;
+}
 const ProductDisplay = () => {
 
     const [data, setData] = React.useState<Product[]>([])
+    const [user, setUser] = React.useState<User[]>([])
     React.useEffect(() => {
         db.products.list().then((res) => {
             setData(res.documents as [])
         }).catch((err) => {
             console.log(err)
         })
+        db.users.list().then((res) => {
+            setUser(res.documents as [])
+        }).catch((err) => {
+            console.log(err)
+        })
     }, [])
     return (
-        <div>
-            {data.map((product, index) => (
-                <div key={index} className='border border-gray-200 p-4'>
-                    <h1 className='text-2xl font-bold'>{product.name}</h1>
-                    <p className='text-lg'>{product.description}</p>
-                    <p className='text-lg font-bold'>${product.price}</p>
-                    {/* <img src={product.image} alt={product.name} className='w-full h-64 object-cover' /> */}
-                </div>
-            ))}
+        <div className='grid grid-flow-col md:grid-cols-3 gap-4 place-content-center w-full'>
+            <div className="container">
+                {data.map((product, index) => (
+                    <div key={index} className='border border-gray-200 p-4'>
+                        <h1 className='text-2xl font-bold'>{product.name}</h1>
+                        <p className='text-lg'>{product.description}</p>
+                        <p className='text-lg font-bold'>${product.price}</p>
+                        {/* <img src={product.image} alt={product.name} className='w-full h-64 object-cover' /> */}
+                    </div>
+                ))}
+            </div>
+            <div className="container">
+
+                {
+                    user && user.map((user, index) => (
+                        <div key={index} className='border border-gray-200 p-4'>
+                            <h1 className='text-2xl font-bold'>{user.name}</h1>
+                            <p className='text-lg'>{user.email}</p>
+                            <p className='text-lg font-bold'>{user.LoggedIn ? 'Logged In' : 'Logged Out'}</p>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
