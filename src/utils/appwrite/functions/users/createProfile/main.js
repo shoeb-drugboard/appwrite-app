f860158ead465c6a4bee7d24f8fb4605b2391b77
shoeb-drugboard <frontend@drugboard.ai>
2025-02-26 18:28:30 +0530

    feat: integrate Appwrite function for user profile creation in RegisterForm

--- src/utils/appwrite/functions/users/createProfile/main.js
+++ src/utils/appwrite/functions/users/createProfile/main.js
@@ -1,47 +1,53 @@
-import { Client, Account, Databases, Functions } from 'node-appwrite';
+import { Client, Account, Databases, Functions, ID } from 'node-appwrite';
 
 export default async ({ req, res, log, error }) => {
-    log(req.body)
-    const data = JSON.parse(req.body)
-    log(data, "Request Body")
-    // log(userId, userName, userEmail);
-    const client = new Client();
-    client.setEndpoint(process.env.APPWRITE_ENDPOINT).setProject(process.env.APPWRITE_PROJECT_ID);
-    client.setKey(process.env.APPWRITE_API_KEY);
-    const db = new Databases(client);
-    if (req.method === "POST") {
-        log("POST request")
+    // log(req.body)
+    // const data = JSON.parse(req.body)
+    // log(data, "Request Body")
+    if (req.body) {
+        const { email, name, LoggedIn } = req.body;
+        // log(userId, userName, userEmail);
+        const client = new Client();
+        client.setEndpoint(process.env.APPWRITE_ENDPOINT).setProject(process.env.APPWRITE_PROJECT_ID);
+        client.setKey(process.env.APPWRITE_API_KEY);
+        const db = new Databases(client);
+        // if (req.method === "POST") {
+        //     log("POST request")
+        // } else {
+        //     log("Not a POST request")
+        // }
+        try {
+            await db.createDocument(
+                process.env.APPWRITE_DB_ID,
+                process.env.APPWRITE_USR_COLLECTION_ID,
+                ID.unique(),
+                {
+                    name: name,
+                    email: email,
+                    LoggedIn: LoggedIn,
+                    createdAt: Date.now(),
+                    updatedAt: Date.now()
+
+                }
+            )
+            log("Profile created successfully", userId)
+        } catch (err) {
+            error("Error creating profile", err)
+            return res.json({
+                message: "error",
+                info: err,
+            })
+        }
+        return res.json({
+            message: "success",
+            // info: { userId, userName, userEmail },
+        })
     } else {
-        log("Not a POST request")
+        return res.json({
+            message: "error",
+            info: "No data found in request body",
+        })
     }
-    // const profile = new Functions(client);
 
-    // try {
-    //     await db.createDocument(
-    //         process.env.APPWRITE_DB_ID,
-    //         process.env.APPWRITE_USR_COLLECTION_ID,
-    //         userId,
-    //         {
-    //             userId,
-    //             name: userName,
-    //             email: userEmail,
-    //             LoggedIn: false,
-    //             createdAt: Date.now(),
-    //             updatedAt: Date.now()
-
-    //         }
-    //     )
-    //     log("Profile created successfully", userId)
-    // } catch (err) {
-    //     error("Error creating profile", err)
-    //     return res.json({
-    //         message: "error",
-    //         info: err,
-    //     })
-    // }
-    return res.json({
-        message: "success",
-        // info: { userId, userName, userEmail },
-    })
 
 }
