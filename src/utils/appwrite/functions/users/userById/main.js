fd1b114816efbdbf420a7c31a3572dc9d584cd3f
shoeb-drugboard <frontend@drugboard.ai>
2025-02-26 16:05:19 +0530

    refactor: update userById function to use context object for improved readability

--- src/utils/appwrite/functions/users/userById/main.js
+++ src/utils/appwrite/functions/users/userById/main.js
@@ -1,8 +1,10 @@
-export default async ({ req, res, log, error }) => {
-    log("Hello World")
-    console.log("Hello World")
-    req.body = JSON.parse(req.body)
-    return res.json({
+//context arg concsists of all data passed to the function
+//we can also destucture the context object to get the req and res objects
+// export default async ({ req, res, log, error }) => {
+export default async (context) => {
+    context.log("Hello World")
+    context.req.body = JSON.parse(req.body)
+    return context.res.json({
         message: "success",
         info: req.body,
         errors: null
