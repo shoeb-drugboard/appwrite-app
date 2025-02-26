b98630c02c87b835e7f70cc0728d1220c0c61830
shoeb-drugboard <frontend@drugboard.ai>
2025-02-26 16:05:03 +0530

    feat: implement profile creation in createProfile function

--- src/utils/appwrite/functions/users/createProfile/main.js
+++ src/utils/appwrite/functions/users/createProfile/main.js
@@ -1,4 +1,4 @@
-import { Client, Account, Databases } from 'node-appwrite';
+import { Client, Account, Databases, Functions } from 'node-appwrite';
 
 export default async ({ req, res, log, error }) => {
     const { userId, userName, userEmail } = JSON.parse(req.payload);
@@ -6,7 +6,32 @@ export default async ({ req, res, log, error }) => {
     const client = new Client();
     client.setEndpoint(process.env.APPWRITE_ENDPOINT).setProject(process.env.APPWRITE_PROJECT_ID);
     client.setKey(process.env.APPWRITE_API_KEY);
+    const db = new Databases(client);
+    // const profile = new Functions(client);
 
+    try {
+        await db.createDocument(
+            process.env.APPWRITE_DB_ID,
+            process.env.APPWRITE_USR_COLLECTION_ID,
+            userId,
+            {
+                userId,
+                name: userName,
+                email: userEmail,
+                LoggedIn: false,
+                createdAt: Date.now(),
+                updatedAt: Date.now()
+
+            }
+        )
+        log("Profile created successfully", userId)
+    } catch (err) {
+        error("Error creating profile", err)
+        return res.json({
+            message: "error",
+            info: err,
+        })
+    }
     return res.json({
         message: "success",
         info: { userId, userName, userEmail },
